a6585622ebf4ba403c0541da0f403c1c98793d63
ang <B022210151@utem.edu.my>
2024-03-19 15:05:10 +0800

    test500

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3000;
 app.use(express.json())
 
 app.get('/', (req, res) => {
-   res.send('EMT best waifu')
+   res.send('Hello world')
 })
 
 app.listen(port, () => {
